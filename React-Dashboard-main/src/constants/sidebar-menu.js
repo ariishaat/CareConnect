185548a7ce65c372f61ba171a98b69f2656336ae
Parth Miglani <parthmiglani08@gmail.com>
2023-03-18 22:58:04 -0400

    add admin ui

---
+++ React-Dashboard-main/src/constants/sidebar-menu.js
@@ -0,0 +1,28 @@
+import DashboardIcon from '../assets/icons/dashboard.svg';
+import ShippingIcon from '../assets/icons/shipping.svg';
+import ProductIcon from '../assets/icons/product.svg';
+import UserIcon from '../assets/icons/user.svg';
+import GroupIcon from '../assets/icons/Group.jpeg';
+
+const sidebar_menu = [
+    {
+        id: 1,
+        icon: DashboardIcon,
+        path: '/',
+        title: 'Dashboard',
+    },
+    {
+        id: 2,
+        icon: GroupIcon,
+        path: '/orders',
+        title: 'Patients',
+    },
+    {
+        id: 3,
+        icon: UserIcon,
+        path: '/profile',
+        title: 'My account',
+    }
+]
+
+export default sidebar_menu;
